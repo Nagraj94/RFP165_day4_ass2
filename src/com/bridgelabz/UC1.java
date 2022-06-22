99d23f8ad5c850dadcbc4c6306772a9f6ba3f07c
Ngowda94 <Nagraj94>
2022-06-22 12:26:24 +0530

    start from position 0

---
+++ src/com/bridgelabz/UC1.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class UC1 {
+    Scanner sc = new Scanner(System.in);
+    void start()
+    {
+        System.out.println("enter to start");
+        int enter = sc.nextInt();
+        int pos = 0;
+    }
+    public static void main(String[] args) {
+        UC1 s = new UC1();
+        s.start();
+    }
+}
