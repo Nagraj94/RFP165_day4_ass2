35b52895321c92aa168c75a4c06ce26856539353
Ngowda94 <Nagraj94>
2022-06-24 20:49:14 +0530

    rolling die to get dice value

--- src/com/bridgelabz/UC1.java
+++ src/com/bridgelabz/UC1.java
@@ -5,21 +5,30 @@ import java.util.Scanner;
 
 public class UC1 {
     Scanner sc = new Scanner(System.in);
+    int dieRoll()
+    {
+        int n = 0;
+        Random r = new Random();
+        n = r.nextInt(7);
+        return (n == 0 ? 1 : n);
+    }
     void start()
     {
-        System.out.println("enter to start");
-        int enter = sc.nextInt();
+        String str = "r";
+        int player1Position = 0;
+        int diceValue = 0;
+        System.out.println("Press 'r' to roll Dice");
+        str = sc.next();
+        diceValue = dieRoll();
+        System.out.println("your dice value is: " + diceValue);
+        player1Position = diceValue;
+        if (str.equals("r"))
+            System.out.println("player is at "+ player1Position + " position");
     }
-    public int dies()
-    {
-        int n=0;
-        Random r = new Random(7);
-        n = r.nextInt();
-        return(n==0?1:n);
-    }
+
     public static void main(String[] args) {
-        UC1 s = new UC1();
-        s.start();
-        s.dies();
+        UC1 uc1 = new UC1();
+        uc1.start();
+
     }
 }
